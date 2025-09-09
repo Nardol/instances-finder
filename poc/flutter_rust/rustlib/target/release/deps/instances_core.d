f1174a95d4b75ffcabbb092ad1374cfd5de3dc73
Patrick ZAJDA <patrick@zajda.fr>
2025-09-09 16:24:55 +0200

    revert(poc): remove Flutter+Rust POC and its documentation

--- poc/flutter_rust/rustlib/target/release/deps/instances_core.d
+++
@@ -1,7 +0,0 @@
-/home/pzajda/instances_social/instances-finder/poc/flutter_rust/rustlib/target/release/deps/instances_core.d: src/lib.rs
-
-/home/pzajda/instances_social/instances-finder/poc/flutter_rust/rustlib/target/release/deps/libinstances_core.so: src/lib.rs
-
-src/lib.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.0.1
--- poc/flutter_rust/rustlib/target/release/libinstances_core.d
+++
@@ -1,1 +0,0 @@
-/home/pzajda/instances_social/instances-finder/poc/flutter_rust/rustlib/target/release/libinstances_core.so: /home/pzajda/instances_social/instances-finder/poc/flutter_rust/rustlib/src/lib.rs
